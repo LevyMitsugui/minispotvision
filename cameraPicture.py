bcb1a6758d20621da16b38a7312f5a620e90c71d
LevyMitsugui <levy1423@gmail.com>
2025-02-04 15:52:59 +0000

    Refactor

--- cameraPicture.py
+++ cameraPicture.py
@@ -3,6 +3,22 @@ import numpy as np
 import os
 
 cam_calib_path = "./camera_calibration.txt"
+
+def undistort_frame(frame, mtx, dist):
+    
+    if len(frame.shape) == 3:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    h,  w = frame.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+    # undistort
+    dst = cv2.undistort(frame, mtx, dist, None, newcameramtx)
+    
+    # crop the image
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+    return dst
+
 def get_cam_calib(calib_path):
     try:
         calib_file = open(cam_calib_path, "r")
@@ -40,35 +56,20 @@ mtx, dist = get_cam_calib(cam_calib_path)
 
 #Open the camera
 cap = cv2.VideoCapture(0)
-
 if not cap.isOpened():
     print("Error: Could not open camera.")
     exit()
 
-# Capture a single frame
 ret, frame = cap.read()
-print("Frame Size:", frame.shape)
-
 if ret:
-    # Save the frame as an image file
+    
     image_path = "captured_image.jpg"
     cv2.imwrite(image_path, frame)
     print(f"Image saved as {image_path}")
 else:
     print("Error: Couldn't capture frame.")
 
-# Release the camera
 cap.release()
 
-
-gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-h,  w = gray.shape[:2]
-newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-# undistort
-dst = cv2.undistort(gray, mtx, dist, None, newcameramtx)
- 
-# crop the image
-x, y, w, h = roi
-dst = dst[y:y+h, x:x+w]
+dst = undistort_frame(frame, mtx, dist)
 cv2.imwrite('calibresult.png', dst)
